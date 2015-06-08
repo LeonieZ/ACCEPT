6ec24e611a6aea7492d2576c329a65bff4689948
vandalum <vandalum@gmail.com>
2015-06-08 16:47:02 +0200

    Turned workflow_objects to a cell array

--- algorithm/workflow.m
+++ algorithm/workflow.m
@@ -1,11 +1,39 @@
-classdef workflow
+classdef workflow < handle
     %WORKFLOW Summary of this class goes here
     %   Detailed explanation goes here
     
     properties
+        algorithm=cell(0);
     end
     
     methods
+        function obj=workflow(savedWorkflow)
+            if isa(savedWorkflow,'workflow_object')
+                obj.algorithm=savedWorkflow;
+            else
+                %give error message
+                
+            end
+        end
+        
+        function returnFrame=run_workflow(self,data)
+            if isempty(self.algorithm)
+                %some kind of logging
+            else
+            for i=1:numel(self.algorithm)
+                data=self.algorithm{i}.run(data);
+            end
+            returnFrame=data;
+            end
+        end
+        
+        function  add_workflow_object(self,object)
+            if ~isa(object,'workflow_object')
+                error('cannot add non workflow_objects to algorithm')                
+            end
+            self.algorithm{end+1}=object;
+        end
+        
     end
     
 end
--- algorithm/workflow_object.m
+++ algorithm/workflow_object.m
@@ -1,86 +1,17 @@
 classdef workflow_object < handle
-    %UNTITLED6 Summary of this class goes here
-    %   Detailed explanation goes here
+    %workflow_object is the base class for all steps in analysis.
+    % Workflow_object provides the basic functions needed by all image
+    % analysis stepps classes such as logging, interaction with the gui,
+    % etc..  
     
     properties
-      Data
+
+      
     end
    
-   properties(SetAccess = private)
-      Next = dlnode.empty;
-      Prev = dlnode.empty;
-   end
-   
-   methods
-      function node = dlnode(Data)
-         % Construct a dlnode object.
-         if nargin > 0
-            node.Data = Data;
-         end
-      end
-      
-      function insertAfter(newNode, nodeBefore)
-         % Insert newNode after nodeBefore.
-         removeNode(newNode);
-         newNode.Next = nodeBefore.Next;
-         newNode.Prev = nodeBefore;
-         if ~isempty(nodeBefore.Next)
-            nodeBefore.Next.Prev = newNode;
-         end
-         nodeBefore.Next = newNode;
-      end
-      
-      function insertBefore(newNode, nodeAfter)
-         % Insert newNode before nodeAfter.
-         removeNode(newNode);
-         newNode.Next = nodeAfter;
-         newNode.Prev = nodeAfter.Prev;
-         if ~isempty(nodeAfter.Prev)
-            nodeAfter.Prev.Next = newNode;
-         end
-         nodeAfter.Prev = newNode;
-      end
-      
-      function removeNode(node)
-         % Remove a node from a linked list.
-         if ~isscalar(node)
-            error('Input must be scalar')
-         end
-         prevNode = node.Prev;
-         nextNode = node.Next;
-         if ~isempty(prevNode)
-            prevNode.Next = nextNode;
-         end
-         if ~isempty(nextNode)
-            nextNode.Prev = prevNode;
-         end
-         node.Next = dlnode.empty;
-         node.Prev = dlnode.empty;
-      end
-      
-      function clearList(node)
-         % Clear the list before
-         % clearing list variable
-         prev = node.Prev;
-         next = node.Next;
-         removeNode(node)
-         while ~isempty(next)
-            node = next;
-            next = node.Next;
-            removeNode(node);
-         end
-         while ~isempty(prev)
-            node = prev;
-            prev = node.Prev;
-            removeNode(node)
-         end
-      end
-   end % methods
-   
-   methods (Access = private)
-      function delete(node)
-         % Delete all nodes
-         clearList(node)
-      end
-   end % private methods
+    methods
+        function returnFrame = run(dataFrame)
+            returnFrame=dataFrame;
+        end
+    end
 end % classdef
