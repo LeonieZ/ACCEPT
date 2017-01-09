07681f28aa9dbfcd177ddbc2f574f3ba0b5af6ed
LeonieZ <l.l.zeune@utwente.nl>
2017-01-09 12:26:07 +0100

    plot function for empty samples

--- misc/plot_locations.m
+++ misc/plot_locations.m
@@ -4,12 +4,14 @@
 [file,dir]=uigetfile('*.mat','Please select a ACCEPT cartridge result');
 load(fullfile(dir,file));
 locations=[];
-locationsInUm=[];
+% locationsInUm=[];
 for i=1:size(currentSample.priorLocations,1)
     locations(i,:)=IO.calculate_overview_location(currentSample,i);
-    locationsInUm(i,:)=IO.calculate_location(currentSample,i);
+    %locationsInUm(i,:)=IO.calculate_location(currentSample,i);
 end
 figure
 imshow(currentSample.overviewImage(:,:,2),parula(4000))
 hold
-scatter(locations(:,2),locations(:,1),'red')
+if ~isempty(locations)
+    scatter(locations(:,2),locations(:,1),'red')
+end
