dee4e8adfcb54df4971cb68437ea5276e7a9cc67
Guus van Dalum <vandalum@gmail.com>
2017-01-27 08:58:18 +0100

    modified the Default loader so it can deal with the deparray images

--- input_output/Loaders/DepArray.m
+++ input_output/Loaders/DepArray.m
@@ -1,4 +1,4 @@
-classdef DepArray < Loader
+classdef DepArray < Loader & IcyPluginData
     %DEFAULT Summary of this class goes here
     %   Detailed explanation goes here
     
@@ -7,13 +7,47 @@ classdef DepArray < Loader
         channelNames='Unknown' 
         channelEdgeRemoval=1;
         hasEdges='false'
-        pixelSize=1
+        pixelSize=0.64
         sample
     end
     
     methods
+        function this=DepArray(input)
+            if nargin == 1
+                validateattributes(input,{'Sample','char'},{'nonempty'},'','input');
+                if isa(input,'Sample')
+                    if strcmp(input.type,this.name)
+                        this.sample=input;
+                    else
+                    error('tried to use incorrect sampletype with DepArray Loader');
+                    end
+                else
+                    this=this.new_sample_path(input);
+                end
+            end
+            
+        end
         function new_sample_path(this,samplePath)
-        
+            this.sample=Sample();
+            this.sample.type = this.name;
+            this.sample.loader = @DepArray;
+            splitPath = regexp(samplePath, filesep, 'split');
+            if isempty(splitPath{end})
+                this.sample.id=splitPath{end-1};
+            else
+                this.sample.id=splitPath{end};
+            end
+            this.sample.pixelSize = this.pixelSize;
+            this.sample.hasEdges = this.hasEdges;
+            this.sample.channelEdgeRemoval = this.channelEdgeRemoval;
+%             customChannelsUsed=this.look_for_custom_channels(samplePath);
+%             if ~isempty(customChannelsUsed)
+%                 this.channelsUsed=customChannelsUsed;
+%             end
+            this.load_scan_info(samplePath);
+            this.preload_tiff_headers(samplePath);
+            this.sample.priorLocations = this.prior_locations_in_sample(samplePath);
+            this.calculate_frame_nr_order();
         end
         
         function update_prior_infos(this,currentSample,samplePath)
@@ -36,7 +70,18 @@ classdef DepArray < Loader
         function bool = can_load_this_folder(path)
             %function that must be present in all loader types to test
             %if the current sample can be loaded by this class.         
+            [txtDir,dirFound]=Loader.find_dir(path,'txt',1);
+            if dirFound
+                tempTxtFileNames = dir([txtDir filesep '*.txt']);
+                for i=1:numel(tempTxtFileNames)
+                    nameArray{i}=tempTxtFileNames(i).name;
+                end
+                test=strcmp(nameArray(:),'Parameters.txt');
+                bool=any(test);
+            else
                 bool = false;
+            end
+                
         end    
     end
 end
