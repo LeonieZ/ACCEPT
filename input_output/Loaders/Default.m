d09ed1b503e8efbce9bc0a1c89f4c126871aac3c
Guus <vandalum@gmail.com>
2017-01-27 20:24:07 +0100

    bug fix

--- input_output/Loaders/Default.m
+++ input_output/Loaders/Default.m
@@ -60,7 +60,7 @@ classdef Default < Loader & IcyPluginData & CustomCsv
         function update_prior_infos(this,currentSample,samplePath)
             this.sample = currentSample;
             if ~exist(currentSample.imagePath,'dir')
-                this.load_scan_info(samplePath);
+                %this.load_scan_info(samplePath);
                 this.preload_tiff_headers(samplePath);         
             end
             this.sample.priorLocations = this.prior_locations_in_sample(samplePath);
