7904124327a3f8c2a83097d50d3ff8178ec69d57
LeonieZ <l.l.zeune@utwente.nl>
2017-02-13 08:20:49 +0100

    plot locations adaptions

--- misc/plot_locations.m
+++ misc/plot_locations.m
@@ -4,12 +4,14 @@
 [file,dir]=uigetfile('*.mat','Please select a ACCEPT cartridge result');
 load(fullfile(dir,file));
 
+thumbContainer = ThumbContainer(currentSample);
+
 locations=[];
 for i=1:size(currentSample.priorLocations,1)
     locations(i,:)=IO.calculate_overview_location(currentSample,i);
 end
 figure
-imshow(currentSample.overviewImage(:,:,2),parula(4000))
+imshow(thumbContainer.overviewImage(:,:,2),parula(4000))
 hold on
 if ~isempty(locations)
     scatter(locations(:,2),locations(:,1),'red')
