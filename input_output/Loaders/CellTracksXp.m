9bed260213e719474e3e5dc85cfd0ff925a4ffb2
Guus van Dalum <vandalum@gmail.com>
2017-04-21 13:00:25 +0200

    small bug fixes for celltracks xp
    There is still some trouble with the correct delimiter character

--- input_output/Loaders/CellTracksXp.m
+++ input_output/Loaders/CellTracksXp.m
@@ -1,3 +1,22 @@
+%% This file is part of ACCEPT, 
+% A program for the automated classification, enumeration and 
+% phenotyping of Circulating Tumor Cells.
+% Copyright (C) 2016 Leonie Zeune, Guus van Dalum, Christoph Brune
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%% 
+    
 classdef CellTracksXp < Loader
     %CELLSEARCH Summary of this class goes here
     %   Detailed explanation goes here
@@ -314,11 +333,13 @@ classdef CellTracksXp < Loader
                 this.dlmData.camXSize=1036;
 
                 this.dlmData.frame=this.read_pic_dlm([this.sample.priorPath filesep 'pic.dlm']);
-                this.dlmData.priorEvents=this.read_cells_dlm([this.sample.priorPath filesep 'cells.dlm'],this.dlmData.frame);
+                if ~isempty(this.dlmData.frame.frameNr)
+                    this.dlmData.priorEvents=this.read_cells_dlm([this.sample.priorPath filesep 'cells.dlm'],this.dlmData.frame);
+                    this.dlmData.num_events = numel(this.dlmData.priorEvents.eventNr);
+                    this.dlmData.CellSearchIds = this.dlmData.priorEvents.eventNr;
+                    this.dlmData.score=this.dlmData.priorEvents.selected;
+                end
                 
-                this.dlmData.num_events = numel(this.dlmData.priorEvents.eventNr);
-                this.dlmData.CellSearchIds = this.dlmData.priorEvents.eventNr;
-                this.dlmData.score=this.dlmData.priorEvents.selected;
                     
                 %notify(this,'logMessage',logmessage(2,['unable to read xml']));
                 
@@ -357,7 +378,7 @@ classdef CellTracksXp < Loader
         
         function priorEvents=read_cells_dlm(this,path,frame)
             fileId=fopen(path);
-            temp=textscan(fileId,'%s%s%f%f%f%f%s%s%s%s%s%s%s','delimiter','?');
+            temp=textscan(fileId,'%s%s%f%f%f%f%s%s%s%s%s%s%s','delimiter','¦');
             nrOfEvents=numel(temp{1});
             priorEvents.eventNr=zeros(nrOfEvents,1);
             priorEvents.selected=zeros(nrOfEvents,1);
@@ -396,7 +417,7 @@ classdef CellTracksXp < Loader
         
         function frame=read_pic_dlm(~,path)
             fileId=fopen(path);
-            temp=textscan(fileId,'%s%s%s%f%s','delimiter','?');
+            temp=textscan(fileId,'%s%s%s%f%s','delimiter','¦');
             frame.timeStamp=temp{1};
             frame.frameNr=temp{4};
             frame.timeStamp=strrep(frame.timeStamp,'"','');
