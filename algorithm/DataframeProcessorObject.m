1c0b3cff8e0cabab40c8057f137e76558f94da1b
LeonieZ <l.l.zeune@utwente.nl>
2018-07-12 14:32:37 +0200

    add comment

--- algorithm/DataframeProcessorObject.m
+++ algorithm/DataframeProcessorObject.m
@@ -17,7 +17,10 @@
 % along with this program.  If not, see <http://www.gnu.org/licenses/>.
 %% 
 classdef DataframeProcessorObject < handle
-    % DataframeProcessorObject is the base class for all steps in dataframe analysis. 
+    % DataframeProcessorObject is the base class for all steps in dataframe analysis.
+    % DataframeProcessorObject provides the basic functions needed by all image
+    % analysis steps classes such as logging, interaction with the gui,
+    % etc..  
     
     properties  
     end
